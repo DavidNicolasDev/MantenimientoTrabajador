8f56951ad6fcd975211bb08912dfd619d9b4f652
DavidNicolasDev <richarddavidnicolas@gmail.com>
2025-06-25 17:54:58 -0500

    Project created

---
+++ MantenimientoTrabajadores/Models/TrabajadoresDataContext.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MantenimientoTrabajadores.Models;
+
+public partial class TrabajadoresDataContext : DbContext
+{
+    public TrabajadoresDataContext()
+    {
+    }
+
+    public TrabajadoresDataContext(DbContextOptions<TrabajadoresDataContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Distrito> Distritos { get; set; }
+
+    public virtual DbSet<Provincia> Provincia { get; set; }
+
+    public virtual DbSet<Trabajadores> Trabajadores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=NICOLAS; Database=TrabajadoresData; Trusted_Connection=True; TrustServerCertificate=True ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departam__3214EC0731391A88");
+
+            entity.ToTable("Departamento");
+
+            entity.Property(e => e.NombreDepartamento)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Distrito>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Distrito__3214EC07B3C5240B");
+
+            entity.ToTable("Distrito");
+
+            entity.Property(e => e.NombreDistrito)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdProvinciaNavigation).WithMany(p => p.Distritos)
+                .HasForeignKey(d => d.IdProvincia)
+                .HasConstraintName("FK__Distrito__IdProv__3D5E1FD2");
+        });
+
+        modelBuilder.Entity<Provincia>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Provinci__3214EC077CB07EA1");
+
+            entity.Property(e => e.NombreProvincia)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdDepartamentoNavigation).WithMany(p => p.Provincia)
+                .HasForeignKey(d => d.IdDepartamento)
+                .HasConstraintName("FK__Provincia__IdDep__3E52440B");
+        });
+
+        modelBuilder.Entity<Trabajadores>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Trabajad__3214EC07E95F5309");
+
+            entity.Property(e => e.Nombres)
+                .HasMaxLength(500)
+                .IsUnicode(false);
+            entity.Property(e => e.NumeroDocumento)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Sexo)
+                .HasMaxLength(1)
+                .IsUnicode(false);
+            entity.Property(e => e.TipoDocumento)
+                .HasMaxLength(3)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdDepartamentoNavigation).WithMany(p => p.Trabajadores)
+                .HasForeignKey(d => d.IdDepartamento)
+                .HasConstraintName("FK__Trabajado__IdDep__3F466844");
+
+            entity.HasOne(d => d.IdDistritoNavigation).WithMany(p => p.Trabajadores)
+                .HasForeignKey(d => d.IdDistrito)
+                .HasConstraintName("FK__Trabajado__IdDis__403A8C7D");
+
+            entity.HasOne(d => d.IdProvinciaNavigation).WithMany(p => p.Trabajadores)
+                .HasForeignKey(d => d.IdProvincia)
+                .HasConstraintName("FK__Trabajado__IdPro__412EB0B6");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MantenimientoTrabajadores/Models/ViewModels/TrabajadorViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MantenimientoTrabajadores.Models.ViewModels
+{
+    public class TrabajadorViewModel
+    {
+        [Required]
+        [Display(Name= "IdTrabajador")]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Nombres { get; set; }
+
+
+        public string? TipoDocumento { get; set; }
+
+        public string? NumeroDocumento { get; set; }
+
+        public string? Sexo { get; set; }
+
+        [Required]
+        public int? IdDepartamento { get; set; }
+
+        [Required]
+        public int? IdProvincia { get; set; }
+
+        [Required]
+        public int? IdDistrito { get; set; }
+
+    }
+}
